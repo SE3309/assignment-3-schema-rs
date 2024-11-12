5a40fedc163d35fa4cf4ff64ef9130f1840f96e3
Chidubem <canekwu2@uwo.ca>
2024-11-12 18:32:59 -0500

    renamed file

--- SQL/CustomerBankCard.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE CustomerBankCard ( 
-    CustomerId INT, 
-    CardNumber VARCHAR(19) NOT NULL, 
-    PRIMARY KEY (CustomerId,CardNumber),
-    FOREIGN KEY (CustomerId) REFERENCES Customer(UserId),
-    FOREIGN KEY (CardNumber) REFERENCES BankCard(CardNumber)
-    );
