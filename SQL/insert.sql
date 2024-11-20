3e5971435bd22a4ea203f9090555ce55ada0081f
Chidubem <canekwu2@uwo.ca>
2024-11-20 13:05:36 -0500

    req labeled in insert and query file

--- SQL/insert.sql
+++ SQL/insert.sql
@@ -1,3 +1,5 @@
-INSERT INTO Customer VALUES(FName,LName,Email,UserPassword)
+-- Need 3 insert statements with 2 of them being more interesting (nested query)
+
+-- INSERT INTO Customer VALUES(FName,LName,Email,UserPassword)
 
 INSERT INTO Customer VALUES("Jonathan","Bailey","Jhardry34@gmail.com","Incredible&9s")
---
+++ SQL/query.sql
@@ -0,0 +1,3 @@
+-- Need 7 query statements and only 1 can be simple 
+
+SELECT fName FROM CUSTOMER WHERE userId=10;
