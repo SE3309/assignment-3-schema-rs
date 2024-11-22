e4550a03e26c2db83b929534199105e07e51fe23
SharafSyed <sharaf.syed04@gmail.com>
2024-11-22 01:10:32 -0500

    added insert statements pending approval

--- SQL/insert.sql
+++ SQL/insert.sql
@@ -3,3 +3,20 @@
 -- INSERT INTO Customer VALUES(FName,LName,Email,UserPassword)
 
 INSERT INTO Customer VALUES("Jonathan","Bailey","Jhardry34@gmail.com","Incredible&9s")
+
+INSERT INTO MenuItem (itemName, itemDescription, pictureUrl, itemPrice, restaurantId)
+SELECT "Chicken Parmesan",
+       "Breaded chicken topped with marinara sauce and melted cheese",
+       "https://www.google.com",
+       12.99,
+       restaurantId
+FROM Restaurant
+WHERE restaurantId = 1;
+
+INSERT INTO CustomerDeliveryAddress (customerId, street, streetNumber, city)
+Select customerId, 
+         "1234 Elm St",
+         "123",
+         "Springfield"
+FROM Customer
+WHERE userId = 1;
