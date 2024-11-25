f674ca05130aa04c7073dec54c2ac50927428648
Sharaf Syed <sharaf.syed04@gmail.com>
2024-11-24 22:54:48 -0500

    added comments to ex6

--- SQL/ex6.sql
+++ SQL/ex6.sql
@@ -1,4 +1,6 @@
 -- Modification 1
+-- The following insert query adds a restaurant specialty to the menu of any restaurant 
+-- that has a menu that is less than 5 items long
 INSERT INTO MenuItem (itemName, itemDescription, pictureUrl, itemPrice, restaurantId)
 SELECT 'Default Item', 'Default Description', 'default_image.jpg', 0.0, restaurantId
 FROM Restaurant
@@ -9,14 +11,17 @@ WHERE restaurantId NOT IN (
     HAVING COUNT(*) > 1
 );
 
+-- turns off safe mode temporarily to be able to update off non-PK attributes
 SET SQL_SAFE_UPDATES = 0;
 
 -- Modification 2
+-- Sets the promotion discount amount to 0% if the promotion has already ended 
 UPDATE promotions
 SET discountPercentage = 0
 WHERE promotionEndDate < NOW();
 
 -- Modification 3
+-- Deletes all reviews that are ranked at 0 stars from all restaurants in a given city (in this case “Lake Markhaven”
 DELETE r1
 FROM review r1
 JOIN restaurant r2 ON r1.restaurantId = r2.restaurantId
