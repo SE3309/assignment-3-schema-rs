d65fde361c37d41740a85f0d6a9e0d12a67fe89c
Scarlett Wu <114163428+swu664@users.noreply.github.com>
2024-11-24 22:18:16 -0500

    change insert to ex3.sql

--- SQL/ex3.sql
+++ SQL/ex3.sql
@@ -1,11 +1,11 @@
--- Need 3 insert statements with 2 of them being more interesting (nested query)
-
+-- Insert 1
 INSERT INTO Customer (fName, lName, email, userPassword) 
 VALUES ("Jonathan", "Bailey", "Jhardry34@gmail.com", "Incredible&9s");
 
 SELECT *
 FROM Customer;
 
+-- Insert 2
 INSERT INTO MenuItem (itemName, itemDescription, pictureUrl, itemPrice, restaurantId)
 SELECT "Chicken Parmesan",
        "Breaded chicken topped with marinara sauce and melted cheese",
@@ -15,6 +15,10 @@ SELECT "Chicken Parmesan",
 FROM Restaurant
 WHERE restaurantId = 1;
 
+SELECT *
+FROM MenuItem;
+
+-- Insert 3
 INSERT INTO CustomerDeliveryAddress (customerId, street, streetNumber, city)
 Select userId, 
          "1234 Elm St",
@@ -22,3 +26,6 @@ Select userId,
          "Springfield"
 FROM Customer
 WHERE userId = 1;
+
+SELECT *
+FROM CustomerDeliveryAddress;
