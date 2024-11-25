4ec86cd443be53f7633f10bf132497fcbb27f723
Scarlett Wu <114163428+swu664@users.noreply.github.com>
2024-11-24 23:15:34 -0500

    add ex5.sql comments

--- SQL/ex5.sql
+++ SQL/ex5.sql
@@ -1,17 +1,21 @@
+-- Query 1
 SELECT * FROM customerdeliveryaddress
 WHERE city = "Alyssaberg";
 
+-- Query 2
 SELECT r.*  
 FROM Restaurant r  
 WHERE (SELECT AVG(rating) AS restRating 
     FROM Review v 
     WHERE v.restaurantId = r.restaurantId) > 4; 
 
+-- Query 3
 SELECT m.*  
 FROM MenuItem m, Restaurant r 
 WHERE r.restaurantName = 'Hungry Bistro of Hut' AND 
       r.restaurantId = m.restaurantId; 
 
+-- Query 4
 SELECT p.* 
 FROM PlacedOrder p, Customer c, Restaurant r, Orderitem o 
 WHERE p.customerId = c.userId AND 
@@ -19,10 +23,12 @@ WHERE p.customerId = c.userId AND
       p.orderId = o.orderId  
 ORDER BY p.orderDate DESC;
 
+-- Query 5
 SELECT r.*
 FROM Restaurant r
 JOIN Promotion p ON r.restaurantId = p.restaurantId;
 
+-- Query 6
 SELECT 
     cbc.cardNumber, 
     SUM(m.itemPrice) + SUM(o.tip) + SUM(o.deliveryFee) AS totalAmount
@@ -35,6 +41,7 @@ JOIN OrderItem o2 ON o2.orderId = o.orderId
 JOIN MenuItem m ON o2.itemName = m.itemName
 GROUP BY cbc.cardNumber;
 
+-- Query 7
 SELECT restaurantName 
 FROM RestaurantGenre g JOIN Restaurant r on g.restaurantId = r.restaurantId
 WHERE genre = "Greek" ;
